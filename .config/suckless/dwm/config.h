ac4c77192d3350e4e21b307193323d5e1e083dc1
Zomboy <zomboy001.mail@tuta.io>
2022-07-15 13:20:34 +0800

    suckless

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 3.5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "fira code nerd font:size=12.5" };
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont};
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
